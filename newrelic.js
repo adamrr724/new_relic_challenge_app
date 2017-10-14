7b5ae7abde7b170e9cebacb70bda26146d2a41b8
Adam Ross Russell <adamrr724@yahoo.com>
2017-10-14 11:25:49 -0700

    added new relic agent

---
+++ newrelic.js
@@ -0,0 +1,25 @@
+'use strict'
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['New Relic Challenge App'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: 'd929825b377192701577acadefa09ac38e47d441',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
